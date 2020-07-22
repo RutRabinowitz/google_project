7579a81e0c4f5110657ec4dbbc6c5934d9dd3fee
RutRabinowitz <tr85330@gmail.com>
2020-07-22 13:50:51 +0300

    init

--- complete.py
+++ complete.py
@@ -2,7 +2,7 @@ import re
 import json
 
 
-class AutoCompleteData:
+class AutoCompletetrie_data:
     def __init__(self, cmpleted_sentence, src_text, offs, scores):
         completed_sentence = cmpleted_sentence
         source_text = src_text
@@ -12,104 +12,56 @@ class AutoCompleteData:
 
 i = 0
 
-data = {}
-
-print(ord('z'))
-# TODO: what about space?
-abc = [chr(c) for c in range(48, 123)]
-abc.append(' ')
-abc.append('$')
-print(ord('1'))
-
-for a in abc:
-    data[a] = {}
-    for b in abc:
-        data[a][b] = {}
-        for c in abc:
-            data[a][b][c] = {}
-            for d in abc:
-                data[a][b][c][d] = []
+trie_data = {}
 
 
-print("Rut Rabinowitz")
-
-with open("rfc7535.txt", "r") as file:
-    dict_ = {}
-    content = file.readlines()
-    # print(content)
-    for line in content:
-        if line[:-1]:
-            i = i + 1
-            dict_[i] = line[:-1]
-            tmp = re.sub('([-()+|&*%#@!/\'\".,:\t\n])+', ' ', line[:-1])
-            tmp = re.sub(' +', ' ', tmp)
-            tmp = tmp.lower()
-            print(tmp)
-            u = 0
-            while(u + 3 < len(tmp)):
-                data[tmp[u]][tmp[u + 1]][tmp[u + 2]][tmp[u + 3]].append(i)
-                if(len(data[tmp[u]][tmp[u + 1]][tmp[u + 2]][tmp[u + 3]]) > 5):
-                    print(tmp[u]+tmp[u + 1]+tmp[u + 2]+tmp[u + 3])
-                u = u + 1
-
-
-
-with open('sentences.json', 'w') as data_file:
-    json.dump(dict_, data_file)
-
-
-
-
-# data = {}
-# json_file = open('sentences.json')
-# sentences = json.load(json_file)
-# for key in sentences.keys():
-
-
-print("end")
-
-user_data = "aargo"
+user_trie_data = "aargo"
 counter = 0
+
+
+def replace_first_char(user_text):
+
 
 
 def replace_char(word_len_5):
     counter = 0
     for i in abc:
-        # data[i][user_data[1]][user_data[2]][user_data[3]][user_data[4]]
-        print(data[i][user_data[1]][user_data[2]][user_data[3]][user_data[4]])
-        print(i + user_data[1] + user_data[2] + user_data[3] + user_data[4])
+        # trie_data[i][user_trie_data[1]][user_trie_data[2]][user_trie_data[3]][user_trie_data[4]]
+        print(trie_data[i][user_trie_data[1]][user_trie_data[2]][user_trie_data[3]][user_trie_data[4]])
+        print(i + user_trie_data[1] + user_trie_data[2] + user_trie_data[3] + user_trie_data[4])
         counter = counter + 1
 
     for i in abc:
-        # data[i][user_data[1]][user_data[2]][user_data[3]][user_data[4]]
-        print(data[user_data[0]][i][user_data[2]][user_data[3]][user_data[4]])
-        print(user_data[0] + i + user_data[2] + user_data[3] + user_data[4])
+        # trie_data[i][user_trie_data[1]][user_trie_data[2]][user_trie_data[3]][user_trie_data[4]]
+        print(trie_data[user_trie_data[0]][i][user_trie_data[2]][user_trie_data[3]][user_trie_data[4]])
+        print(user_trie_data[0] + i + user_trie_data[2] + user_trie_data[3] + user_trie_data[4])
         counter = counter + 1
 
     for i in abc:
-        # data[i][user_data[1]][user_data[2]][user_data[3]][user_data[4]]
-        print(data[user_data[0]][user_data[1]][i][user_data[4]])
-        print(user_data[0] + user_data[1] + i + user_data[3] + user_data[4])
+        # trie_data[i][user_trie_data[1]][user_trie_data[2]][user_trie_data[3]][user_trie_data[4]]
+        print(trie_data[user_trie_data[0]][user_trie_data[1]][i][user_trie_data[4]])
+        print(user_trie_data[0] + user_trie_data[1] + i + user_trie_data[3] + user_trie_data[4])
         counter = counter + 1
 
     for i in abc:
-        # data[i][user_data[1]][user_data[2]][user_data[3]][user_data[4]]
-        print(data[user_data[0]][user_data[1]][user_data[2]][i][user_data[4]])
-        print(user_data[0] + user_data[1] + user_data[2] + i + user_data[4])
+        # trie_data[i][user_trie_data[1]][user_trie_data[2]][user_trie_data[3]][user_trie_data[4]]
+        print(trie_data[user_trie_data[0]][user_trie_data[1]][user_trie_data[2]][i][user_trie_data[4]])
+        print(user_trie_data[0] + user_trie_data[1] + user_trie_data[2] + i + user_trie_data[4])
         counter = counter + 1
 
     for i in abc:
-        # data[i][user_data[1]][user_data[2]][user_data[3]][user_data[4]]
-        print(data[user_data[0]][user_data[1]][user_data[2]][user_data[3]][i])
-        print(user_data[0] + user_data[1] + user_data[2] + user_data[3] + i)
+        # trie_data[i][user_trie_data[1]][user_trie_data[2]][user_trie_data[3]][user_trie_data[4]]
+        print(trie_data[user_trie_data[0]][user_trie_data[1]][user_trie_data[2]][user_trie_data[3]][i])
+        print(user_trie_data[0] + user_trie_data[1] + user_trie_data[2] + user_trie_data[3] + i)
         counter = counter + 1
 
 
-def sub_str(user_data):
+def sub_str(user_trie_data):
     res = []
-    for i in data:
-        if user_data == i[user_data[0]][user_data[1]][user_data[2]][user_data[3]][user_data[4]]:
-            res.append(i[user_data[0]][user_data[1]][user_data[2]][user_data[3]][user_data[4]])
+    for i in trie_data:
+        if user_trie_data == i[user_trie_data[0]][user_trie_data[1]][user_trie_data[2]][user_trie_data[3]][
+            user_trie_data[4]]:
+            res.append(i[user_trie_data[0]][user_trie_data[1]][user_trie_data[2]][user_trie_data[3]][user_trie_data[4]])
 
     return res
 
---
+++ init_data.py
@@ -0,0 +1,110 @@
+import re
+import json
+import os
+
+
+def insert_new_3keys_and_str(trie_data, sentence, index, curr):
+    trie_data[sentence[curr]] = {}
+    trie_data[sentence[curr]][sentence[curr + 1]] = {}
+    trie_data[sentence[curr]][sentence[curr + 1]][sentence[curr + 2]] = {}
+    trie_data[sentence[curr]][sentence[curr + 1]][sentence[curr + 2]][sentence[curr + 3]] = [index]
+    return trie_data
+
+
+def insert_new_2keys_and_str(trie_data, sentence, index, curr):
+    trie_data[sentence[curr]][sentence[curr + 1]] = {}
+    trie_data[sentence[curr]][sentence[curr + 1]][sentence[curr + 2]] = {}
+    trie_data[sentence[curr]][sentence[curr + 1]][sentence[curr + 2]][sentence[curr + 3]] = [index]
+    return trie_data
+
+
+def insert_new_key_and_str(trie_data, sentence, index, curr):
+    trie_data[sentence[curr]][sentence[curr + 1]][sentence[curr + 2]] = {}
+    trie_data[sentence[curr]][sentence[curr + 1]][sentence[curr + 2]][sentence[curr + 3]] = [index]
+    return trie_data
+
+
+def insert_new_str(trie_data, sentence, index, curr):
+    trie_data[sentence[curr]][sentence[curr + 1]][sentence[curr + 2]][sentence[curr + 3]] = [index]
+    return trie_data
+
+
+def insert_str_to_data(trie_data, sentence, index, curr):
+    if sentence[curr] in trie_data.keys():
+        if sentence[curr + 1] in trie_data[sentence[curr]].keys():
+            if sentence[curr + 2] in trie_data[sentence[curr]][sentence[curr + 1]].keys():
+                if sentence[curr + 3] in trie_data[sentence[curr]][sentence[curr + 1]][sentence[curr + 2]].keys():
+                    trie_data[sentence[curr]][sentence[curr + 1]][sentence[curr + 2]][sentence[curr + 3]].append(
+                        index)
+                else:
+                    trie_data = insert_new_str(trie_data, sentence, index, curr)
+            else:
+                trie_data = insert_new_key_and_str(trie_data, sentence, index, curr)
+        else:
+            trie_data = insert_new_2keys_and_str(trie_data, sentence, index, curr)
+    else:
+        trie_data = insert_new_3keys_and_str(trie_data, sentence, index, curr)
+
+    return trie_data
+
+
+def insert_sen_to_trie(trie_data, sentence, index):
+    curr = 0
+    while curr + 3 < len(sentence):
+        trie_data = insert_str_to_data(trie_data, sentence, index, curr)
+        curr = curr + 1
+    return trie_data
+
+
+def fix_sentence(sentence):
+    new_sentence = re.sub('([.,\t\n])+', ' ', sentence[:-1])
+    new_sentence = re.sub(' +', ' ', new_sentence)
+    return new_sentence.lower()
+
+
+def insert_line(trie_data, line, sentence_index):
+    sentence = fix_sentence(line[:-1])
+    trie_data = insert_sen_to_trie(trie_data, sentence, sentence_index)
+    return trie_data
+
+
+def insert_file(file_name, trie_data, sentence_index):
+    with open('RFC/' + file_name, 'r+', encoding="utf8") as current_file:
+        data_to_json_file = {}
+        content = current_file.readlines()
+        for line in content:
+            if line[:-1]:
+                sentence_index = sentence_index + 1
+                data_to_json_file[sentence_index] = line[:-1]
+                trie_data = insert_line(trie_data, line, sentence_index)
+
+        with open('sentences.json', 'w') as sentences_file:
+            json.dump(data_to_json_file, sentences_file)
+
+    return trie_data, sentence_index
+
+
+def insert_sub_dir(files, sentence_index, trie_data):
+    if len(files) > 0:
+        for file in files:
+            trie_data, sentence_index = insert_file(file, trie_data, sentence_index)
+            print("_", end="")
+    return sentence_index, trie_data
+
+
+def init():
+    print("loading files and preparing the systems")
+    sentence_index = 1
+    trie_data = {}
+    subdirectories = [x[0] for x in os.walk('RFC')]
+
+    for subdir in subdirectories:
+        files = os.walk(subdir).__next__()[2]
+        sentence_index, trie_data = insert_sub_dir(files, sentence_index, trie_data)
+
+    with open('trie_data.json', 'w') as trie_file:
+        json.dump(trie_data, trie_file)
+
+
+if __name__ == '__main__':
+    init()
